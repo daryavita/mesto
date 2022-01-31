39dd31001eac57017ab7f6ccb4e6bab2c7731ef5
Darya <popova.d.v@yandex.ru>
2022-01-31 23:03:01 +0300

    feature: добавить валидацию

---
+++ scripts/validate.js
@@ -0,0 +1,61 @@
+
+function submitForm(event) {
+    event.preventDefault();
+    console.log('submit'); 
+}
+
+function validateInput(form, input, {inputErrorClass, errorSelector, errorClass}) {
+    const errorContainer = form.querySelector(errorSelector);
+    console.log(input.validationMessage)
+
+    if (input.validity.valid) {
+        input.classList.remove(inputErrorClass)
+        errorContainer.classList.remove(errorClass);
+        errorContainer.textContent = " ";
+    } else {
+        input.classList.add(inputErrorClass)
+        errorContainer.classList.add(errorClass);
+        errorContainer.textContent = input.validationMessage;
+    }
+}
+
+function enableValidation({ formSelector, inputSelector, ...rest }) {
+    const forms = document.querySelectorAll(formSelector);
+    console.log(forms)
+
+    forms.forEach(form => {
+        form.addEventListener('submit', submitForm);
+
+        const inputs = form.querySelectorAll(inputSelector);
+
+        inputs.forEach(input => {
+            input.addEventListener('input', () => {
+                validateInput(form, input, rest);
+            });
+        })
+    })
+
+     
+};
+
+enableValidation( {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input-info',
+    errorSelector: '.popup__error',
+    inputErrorClass: 'popup__input-info_type_error',
+    errorClass: 'popup__error_visible'
+});
+ 
+
+
+
+
+
+// enableValidation({
+//     formSelector: '.popup__form',
+//     inputSelector: '.popup__input',
+//     submitButtonSelector: '.popup__button',
+//     inactiveButtonClass: 'popup__button_disabled',
+//     inputErrorClass: 'popup__input_type_error',
+//     errorClass: 'popup__error_visible'
+//   }); 
