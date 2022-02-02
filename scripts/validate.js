0c1bab630920021d02a7ca7926f4f618ce237800
Darya <dashkapv@mail.ru>
2022-02-02 15:36:53 +0300

    refactor: разбить функции в JS

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,24 +4,46 @@ function submitForm(event) {
     console.log('submit'); 
 }
 
-function validateInput(form, input, {inputErrorClass, errorClass}) {
-    const errorContainer = form.querySelector(`#${input.id}-error`);
-    console.log(input.validationMessage)
+function showError (input, errorContainer, {inputErrorClass, errorClass}) {
+    input.classList.add(inputErrorClass)
+    errorContainer.classList.add(errorClass);
+    errorContainer.textContent = input.validationMessage;
+}
 
-    if (input.validity.valid) {
-        input.classList.remove(inputErrorClass)
-        errorContainer.classList.remove(errorClass);
-        errorContainer.textContent = " ";
+function hideError (input, errorContainer, {inputErrorClass, errorClass}) {
+    input.classList.remove(inputErrorClass)
+    errorContainer.classList.remove(errorClass);
+    errorContainer.textContent = " ";
+}
+
+function toggleButton (form, {submitButtonSelector, inactiveButtonClass}) {
+    const button = form.querySelector(submitButtonSelector);
+    const isFormValid = form.checkValidity()
+
+    if (isFormValid) {
+        button.classList.remove(inactiveButtonClass);
+        button.removeAttribute('disabled');
     } else {
-        input.classList.add(inputErrorClass)
-        errorContainer.classList.add(errorClass);
-        errorContainer.textContent = input.validationMessage;
+        button.classList.add(inactiveButtonClass);
+        button.setAttribute('disabled', 'disabled');
     }
 }
 
+function validateInput(form, input, classes) {
+    const errorContainer = form.querySelector(`#${input.id}-error`);
+
+    if (input.validity.valid) {
+        hideError(input, errorContainer, classes);
+    } else {
+        showError(input, errorContainer, classes);
+    }
+
+    toggleButton(form, classes)
+}
+
+
 function enableValidation({ formSelector, inputSelector, ...rest }) {
     const forms = document.querySelectorAll(formSelector);
-    console.log(forms)
 
     forms.forEach(form => {
         form.addEventListener('submit', submitForm);
@@ -33,29 +55,16 @@ function enableValidation({ formSelector, inputSelector, ...rest }) {
                 validateInput(form, input, rest);
             });
         })
-    })
 
-     
+        toggleButton(form, rest);
+    })
 };
 
 enableValidation( {
     formSelector: '.popup__form',
     inputSelector: '.popup__input-info',
-    // errorSelector: '.popup__error',
+    submitButtonSelector: '.popup__submit',
     inputErrorClass: 'popup__input-info_type_error',
-    errorClass: 'popup__error_visible'
+    errorClass: 'popup__error_visible',
+    inactiveButtonClass: 'popup__submit_disabled',
 });
- 
-
-
-
-
-
-// enableValidation({
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_disabled',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__error_visible'
-//   }); 
